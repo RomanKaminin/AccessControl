a36b2a08c9596ed3bb0d64c09f84b7e7266047a7
RomanKaminin <roman.kaminin@osinit.com>
2018-10-08 12:51:51 +0300

    login registration

--- django_project/app/forms.py
+++ django_project/app/forms.py
@@ -27,7 +27,6 @@ class UserRegistrationForm(forms.Form):
             attrs={'placeholder': 'Password'}
         )
     )
-
     def clean(self):
         username = self.cleaned_data.get('username')
         email = self.cleaned_data.get('email')
@@ -38,12 +37,12 @@ class UserRegistrationForm(forms.Form):
         return self.cleaned_data
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(
+    email = forms.CharField(
         required = True,
-        label = 'Username',
+        label = 'Email',
         max_length = 32,
         widget=forms.TextInput(
-            attrs={'placeholder': 'Username'}
+            attrs={'placeholder': 'Email'}
         )
     )
     password = forms.CharField(
@@ -54,17 +53,22 @@ class UserLoginForm(forms.Form):
             attrs={'placeholder': 'Password'}
         )
     )
-
     def clean(self):
-        username = self.cleaned_data.get('username')
+        email = self.cleaned_data.get('email')
         password = self.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
-        if not user or not user.is_active:
-            raise forms.ValidationError("Sorry, that login was invalid. Please try again.")
+        try:
+            user = User.objects.get(email=email)
+            if not user.check_password(password):
+                raise forms.ValidationError(u'Incorrect password.')
+            elif not user.is_active:
+                raise forms.ValidationError(u'User with this e-mail is blocked.')
+        except User.DoesNotExist:
+            raise forms.ValidationError(u'User with this e-mail does not exist.')
         return self.cleaned_data
 
     def login(self, request):
-        username = self.cleaned_data.get('username')
+        email = self.cleaned_data.get('email')
         password = self.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
+        user_obj = User.objects.get(email=email)
+        user = authenticate(username=user_obj.username, password=password)
         return user
