dffbef8d702ceb39586afc164fb169c76f8b4112
kamhome <feliks-roman@mail.ru>
2018-11-27 22:06:51 +0300

    filters labels

--- django_project/app/filtersets.py
+++ django_project/app/filtersets.py
@@ -8,5 +8,8 @@ class AccessFilter(django_filters.FilterSet):
         model = AccessRequest
         fields = ['access', 'space_name']
 
-
+    def __init__(self, *args, **kwargs):
+        super(AccessFilter, self).__init__(*args, **kwargs)
+        self.filters['access'].label = "Текущий доступ"
+        self.filters['space_name'].label = "Цель запроса"
 
